cf307819eca6276c6d9da210b914d9c94b475513
Tina H <heyan_75@163.com>
2017-06-19 15:24:01 -0400

    Update script.js

--- script.js
+++ script.js
@@ -23,8 +23,8 @@ $(document).ready(function() {
   $(".animsition").animsition({
     inClass: 'fade-in',
     outClass: 'fade-out',
-    inDuration: 1500,
-    outDuration: 800,
+    inDuration: 600,
+    outDuration: 600,
     linkElement: '.animsition-link',
     // e.g. linkElement: 'a:not([target="_blank"]):not([href^="#"])'
     loading: true,
