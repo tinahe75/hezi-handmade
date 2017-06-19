42c8179ce4bf72c3deb64d7d8b22b1c78026038c
Tina H <heyan_75@163.com>
2017-06-18 22:43:51 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 $(document).ready(function(){
     $('body').css('display','none');
-    $('body').fadeIn(9000);
+    $('body').fadeIn(1000);
 
 $('.menu').click(function(event){
     event.preventDefault();
     newLocation = this.href;
 
-$('body').fadeOut(9000, newpage);
+$('body').fadeOut(1000, newpage);
 
 });
 
