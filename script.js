97e308942b4a6345684a0ccc0a2b515ff4a7e9a3
Tina H <heyan_75@163.com>
2017-06-18 23:03:28 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
 $(document).ready(function(){
-    $('body').css('display','none');
-    $('body').fadeIn(1000);
+    $('.gallery').css('display','none');
+    $('.gallery').fadeIn(1000);
 
 $('.menu a').click(function(event){
     event.preventDefault();
     newLocation = this.href;
 
-$('body').fadeOut(1000, newpage);
+$('.gallery').fadeOut(1000, newpage);
 
 });
 
 function newpage() {
     //window.location = newLocation;
-    $('body').fadeIn(1000);
+    $('.gallery').fadeIn(1000);
 
 }
 
